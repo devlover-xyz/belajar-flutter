7444a4ea10dc291de23cf503870cb41a5b5c4299
Baihaqy Aviq <aviq.baihaqy@gmail.com>
2021-04-24 15:02:34 +0700

    latihan-3

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,104 @@ void main() {
 class RamadhanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Column _buildButtonColoumn(Color color, IconData icon, String label) {
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            color: color,
+          ),
+          Text(
+            label,
+            style: TextStyle(color: color),
+          ),
+        ],
+      );
+    }
+
+    var titleSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 3.0),
+            child: Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+          ),
+          Text('41')
+        ],
+      ),
+    );
+
+    var color = Theme.of(context).primaryColor;
+
+    var buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColoumn(color, Icons.call, 'CALL'),
+          _buildButtonColoumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColoumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    var textSection = Container(
+      padding: EdgeInsets.all(32),
+      child: Text(
+          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+          'Alps. Situated 1,578 meters above sea level, it is one of the '
+          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+          'half-hour walk through pastures and pine forest, leads you to the '
+          'lake, which warms to 20 degrees Celsius in the summer. Activities '
+          'enjoyed here include rowing, and riding the summer toboggan run.'),
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Aplikasi Ramadhan Layout'),
+          title: Text('Aplikasi Latihan Layout'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Text kolom 1"),
-            Text("Text kolom 2"),
-            Text("Text kolom 3"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Text row 1"),
-                Text("Text row 2"),
-                Text("Text row 3"),
-              ],
-            )
+            /// image section
+            Image.asset(
+              'images/photo1.jpeg',
+              width: double.infinity,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+
+            /// title section
+            titleSection,
+
+            /// button section
+            buttonSection,
+
+            /// text section
+            textSection
           ],
         ),
       ),
